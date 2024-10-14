670226c555ebc40a17a9451433c1da1712bc15bc
annas <annassdan@gmail.com>
2024-10-14 07:21:35 +0700

    verify user

--- src/routes/share/+page.svelte
+++ src/routes/share/+page.svelte
@@ -1,4 +1,4 @@
 <script></script>
 <div  class="max-w-[50rem] relative px-4 pt-6 pb-24 flex flex-col items-center h-full bg-gradient-to-b from-wedding-200 to-white text-wedding-600 gap-3 text-sm">
-
+    HAH
 </div>
