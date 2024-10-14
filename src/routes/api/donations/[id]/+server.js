edf10016e1b21559f45614e89b8564af68a559f9
annas <annassdan@gmail.com>
2024-10-14 12:13:23 +0700

    test again 2

--- src/routes/api/donations/[id]/+server.js
+++ src/routes/api/donations/[id]/+server.js
@@ -11,6 +11,7 @@ export async function POST({ params, request, fetch }) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXJyZW5jeSI6IklEUiIsImlkIjoiMzdiOTg2NTgtNGY0Yy00MGE3LTg0NmEtZjc2YjIxMGU2NDRiIiwiZW1haWwiOiJhbm5hc3NkYW5AZ21haWwuY29tIiwidXNlcm5hbWUiOiJNTkFubmFzIiwidGllcl9rZXkiOiJCQVNJQyIsImlzcyI6InNhd2VyaWEtbG9naW4iLCJpYXQiOjE3Mjg3OTAzNzAsImV4cCI6MTcyOTA0OTU3MCwianRpIjoiZTZlYzI5YWUtMDU5NS00MTNmLThjMWQtZWQ5NzAyNjA4MDNlIn0.viXPJa9OFoyNx7tV-VSupQm3DJgPS5t9BRy8z6a-9Ss',
                 // Forward any authorization headers if needed
                 ...request.headers
             },
